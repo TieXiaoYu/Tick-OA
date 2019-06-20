9828c9d14e571fcf75dd78a116f898f327e0d316
txy <1596913818@qq.com>
2019-06-20 20:41:37 +0800

    只剩下修改个人信息功能0.0

--- src/main/java/com/qfedu/controller/StaffController.java
+++ src/main/java/com/qfedu/controller/StaffController.java
@@ -12,7 +12,6 @@ import com.qfedu.vo.ResultVo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 

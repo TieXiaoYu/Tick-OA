50f1bf636ad066cf1679a3cc7db0090e0bf4415d
TieXiaoYu <1596913818@qq.com>
2022-04-18 09:22:14 +0800

    第一次commit

---
+++ src/main/java/com/qfedu/controller/Demo.java
@@ -0,0 +1,13 @@
+package com.qfedu.controller;
+
+/**
+ * @Auther: Tie Xiaoyu
+ * @Date: 2022/4/18 - 04 - 18 - 9:19
+ * @Description: com.qfedu.controller
+ */
+public class Demo {
+
+    public static void main(String[] args) {
+        return;
+    }
+}
